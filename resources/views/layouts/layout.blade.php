aba254036cfbe6131b833a094ef7782e720e162c
fidelanathania <fidelanathania05@gmail.com>
2022-05-27 22:51:08 +0700

    edit view layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,8 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!--<title> Responsive Sidebar Menu  | CodingLab </title>-->
     <link rel="stylesheet" href="{{ asset('css/layouts/style.css') }}">
     @yield('stylesheet')
@@ -23,21 +22,23 @@
     </header>
     <div class="l-navbar" id="nav-bar">
         <nav class="nav">
-            <div> <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span
-                        class="nav_logo-name">KALM</span> </a>
-                <div class="nav_list"> <a href="#" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i>
-                        <span class="nav_name">Dashboard</span> </a>
-                    <a href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span
-                            class="nav_name">Users</span> </a>
-                    <a href="#" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i>
-                        <span class="nav_name">Messages</span> </a> <a href="#" class="nav_link"> <i
-                            class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> <a
-                        href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i>
-                        <span class="nav_name">Files</span> </a> <a href="#" class="nav_link"> <i
-                            class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a>
+            <div> <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">KALM</span> </a>
+                <div class="nav_list">
+                    <a href="#" class="nav_link active"> <i class='bx bxs-home nav_icon'></i>
+                        <span class="nav_name">Home</span> </a>
+                    <a href="#" class="nav_link"> <i class='bx bxs-book-open nav_icon'></i>
+                        <span class="nav_name">Journaling</span> </a>
+                    <a href="#" class="nav_link"> <i class='bx bxs-group nav_icon'></i>
+                        <span class="nav_name">Consulting</span> </a>
+                    <a href="#" class="nav_link"> <i class='bx bxs-leaf nav_icon'></i>
+                        <span class="nav_name">Meditate</span> </a>
+                    <a href="#" class="nav_link"> <i class='bx bxs-conversation nav_icon'></i>
+                        <span class="nav_name">Messages</span> </a>
+                    <a href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i>
+                        <span class="nav_name">Me</span> </a>
                 </div>
             </div> <a class="nav_link" onclick="event.preventDefault(); document.getElementById('logout').submit();
-            "> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
+            "> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">Sign Out</span> </a>
 
             <form hidden action="{{ route('logout') }}" method="POST" id="logout">@csrf</form>
 
@@ -48,8 +49,7 @@
         @yield('content')
     </div>
     <!--Container Main end-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
     <script src="{{ asset('js/layouts/main.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
